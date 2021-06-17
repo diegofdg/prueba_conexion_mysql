7f7f79ec8c284845803d151e843ccab813f8b105
Diego Di Giorgio <diegofdg@gmail.com>
2021-06-17 00:34:04 -0300

    primer commit

--- routes/routes.js
+++ routes/routes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 var categoria = require('../controllers/categoria_controller');
 
 // Creamos la ruta para realizar una consulta
-router.get('/categoria', categoria.getCategoria);
+router.get('/', categoria.getCategoria);
 
 // Exportamos el modulo de las rutas
 module.exports = router;
